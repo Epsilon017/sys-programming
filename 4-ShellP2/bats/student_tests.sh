f34c54538529c575c2fe2fdbdef7cf083fd1ceb7
Epsilon017 <141088741+Epsilon017@users.noreply.github.com>
2025-02-19 23:30:37 -0500

    Update student_tests with complete test cases

--- 4-ShellP2/bats/student_tests.sh
+++ 4-ShellP2/bats/student_tests.sh
@@ -4,11 +4,108 @@
 # 
 # Create your unit tests suit in this file
 
-@test "Example: check ls runs without errors" {
-    run ./dsh <<EOF                
+@test "Execute ls command" {
+    run "./dsh.out" <<EOF                
 ls
 EOF
+    
+    echo "Captured stdout:" 
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    
+    [ "$status" -eq 0 ]
+}
 
-    # Assertions
+@test "Execute uname command" {
+    run "./dsh.out" <<EOF
+uname
+EOF
+
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+    expected_output="Linuxdsh2>dsh2>cmdloopreturned0"
+
+    echo "Captured stdout:"
+    echo "Output: $output"
+    echo "Exit Status: $status"
+
+    [ "$stripped_output" = "$expected_output" ]
+    [ "$status" -eq 0 ]
+}
+
+@test "Execute uname command with arg" {
+    run "./dsh.out" <<EOF
+uname -p
+EOF
+
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+    expected_output="x86_64dsh2>dsh2>cmdloopreturned0"
+
+    echo "Captured stdout:"
+    echo "Output: $output"
+    echo "Exit Status: $status"
+
+    [ "$stripped_output" = "$expected_output" ]
+    [ "$status" -eq 0 ]
+}
+
+@test "Change directory and list contents" {
+    current=$(pwd)
+    cd .
+    mkdir -p dsh-test && cd dsh-test
+    touch testfile
+    
+    run "${current}/dsh.out" <<EOF                
+cd /tmp/dsh-test
+ls
+EOF
+    
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+    expected_output="testfiledsh2>dsh2>dsh2>cmdloopreturned0"
+    
+    echo "Captured stdout:" 
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    
+    [ "$stripped_output" = "$expected_output" ]
+    [ "$status" -eq 0 ]
+}
+
+@test "Random command handling" {
+    run "./dsh.out" <<EOF                
+randomcmd
+EOF
+    
+    echo "Captured stdout:" 
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    
+    [ "$status" -eq 0 ]
+}
+
+@test "Execute echo command" {
+    run "./dsh.out" <<EOF                
+echo Hello, world!
+EOF
+    
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+    expected_output="Hello,world!dsh2>dsh2>cmdloopreturned0"
+    
+    echo "Captured stdout:" 
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    
+    [ "$stripped_output" = "$expected_output" ]
+    [ "$status" -eq 0 ]
+}
+
+@test "Dragon command execution" {
+    run "./dsh.out" <<EOF                
+dragon
+EOF
+    
+    echo "Captured stdout:" 
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    
     [ "$status" -eq 0 ]
 }
