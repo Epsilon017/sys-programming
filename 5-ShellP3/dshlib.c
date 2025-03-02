af8bb1798de8b7008134cfc7dead50e98c3ff529
Epsilon017 <141088741+Epsilon017@users.noreply.github.com>
2025-03-01 19:18:59 -0500

    Add exec_local_cmd_loop implementation

--- 5-ShellP3/dshlib.c
+++ 5-ShellP3/dshlib.c
@@ -6,8 +6,11 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <errno.h>
 
 #include "dshlib.h"
+
+int last_rc = 0;
 
 /*
  * Implement your exec_local_cmd_loop function by building a loop that prompts the 
@@ -54,6 +57,107 @@
  */
 int exec_local_cmd_loop()
 {
+    char cmd_buff[SH_CMD_MAX];
+    cmd_buff_t cmd;
+
+
+    // TODO IMPLEMENT MAIN LOOP
+    while (1) {
+
+        printf("%s", SH_PROMPT);
+
+        if (fgets(cmd_buff, SH_CMD_MAX, stdin) == NULL) { // for running headless
+            printf("\n");
+            break;
+        }
+
+        if (!strchr(cmd_buff, '\n')) {
+
+            // input was way too long, clear input stream and ignore
+            char c = 0;
+            while (c != '\n' && c != EOF) {
+                c = getchar();
+            }
+            continue;
+
+        }
+
+        // remove the trailing \n from cmd_buff
+        cmd_buff[strcspn(cmd_buff, "\n")] = '\0';
+
+        // parse input to cmd_buff_t *cmd_buff
+        clean_input(cmd_buff);
+        clear_cmd_buff(&cmd);
+        int parse_rc = build_cmd_buff(cmd_buff, &cmd);
+
+        if (parse_rc == ERR_TOO_MANY_COMMANDS) {
+            printf(CMD_ERR_PIPE_LIMIT, CMD_MAX);
+            continue;
+        }
+
+        if (parse_rc == WARN_NO_CMDS) {
+            printf(CMD_WARN_NO_CMD);
+            continue;
+        }
+
+        // if built-in command, execute builtin logic for exit, cd (extra credit: dragon)
+        Built_In_Cmds built_in_rc = exec_built_in_cmd(&cmd);
+        if (built_in_rc == BI_EXECUTED) {
+            last_rc = 0;
+            continue;
+        }
+
+        // TODO IMPLEMENT if not built-in command, fork/exec as an external command
+        // for example, if the user input is "ls -l", you would fork/exec the command "ls" with the arg "-l"
+        pid_t fork_rc = fork();
+        if (fork_rc < 0) {
+            continue;
+        }
+
+        if (fork_rc == 0) {
+
+            // child
+            execvp(cmd.argv[0], cmd.argv);
+            exit(errno);
+
+        } else {
+
+            // parent
+            int child_result;
+            wait(&child_result);
+            int child_rc = WEXITSTATUS(child_result);
+            last_rc = child_rc;
+
+            if (child_rc != 0) {
+                switch(child_rc) {
+                    case ENOENT:
+                        printf(CMD_ERR_EXECUTE, "Command not found in PATH\n");
+                        break;
+                    
+                    case EACCES:
+                        printf(CMD_ERR_EXECUTE, "Permission denied\n");
+                        break;
+                    
+                    case ENOTDIR:
+                        printf(CMD_ERR_EXECUTE, "Invalid path\n");
+                        break;
+
+                    case EISDIR:
+                        printf(CMD_ERR_EXECUTE, "Unable to execute directory\n");
+                        break;
+                    
+                    case ENOMEM:
+                        printf(CMD_ERR_EXECUTE, "Not enough memory to execute\n");
+                        break;
+                    
+                    default:
+                        printf(CMD_ERR_EXECUTE, "Execution failed\n");
+                }
+            }
+
+        }
+    
+    }
    
     return OK;
 }
